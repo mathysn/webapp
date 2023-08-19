bd9c174e874ead6dd60d92cbb69df9b539c68dd9
Mathys <mathys.nourry@edu.univ-fcomte.fr>
2023-08-19 23:03:53 +0200

    Register/Login/Logout working and synced with the database

---
+++ database/script.sql
@@ -0,0 +1,21 @@
+CREATE TABLE roles(
+    id_role INT,
+    role_name VARCHAR(255),
+    PRIMARY KEY (id_role)
+);
+
+CREATE TABLE users(
+    id_user INT AUTO_INCREMENT,
+    username VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role_id INT DEFAULT 1,
+    registration_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id_user),
+    FOREIGN KEY (role_id) REFERENCES roles(id_role)
+);
+
+INSERT INTO roles (id_role, role_name) VALUES
+    (1, 'User'),
+    (2, 'Moderator'),
+    (3, 'Administrator');
--- server.js
+++ server.js
@@ -1,15 +1,25 @@
 const express = require('express');
 const axios = require('axios');
 const bodyParser = require('body-parser');
-const mysql = require('mysql');
+const mysql2 = require('mysql2');
 const path = require('path');
 const bcrypt = require('bcrypt')
+const crypto = require('crypto');
+const session = require('express-session');
 
 const captchaKey = require('./captchaKeys.json');
+// const { hash } = require("bcrypt");
 
 const app = express();
 const port = 3000;
 
+const sessionSecretKey = crypto.randomBytes(32).toString('hex');
+
+app.use(session({
+    secret: sessionSecretKey,
+    resave: false,
+    saveUninitialized: true,
+}));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use('/static', express.static(__dirname + '/views/static'));
@@ -21,12 +31,11 @@ app.listen(port, () => {
 
 });
 
-const connection = mysql.createConnection({
+const connection = mysql2.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'root',
     database: 'webappdb'
-
 });
 
 connection.connect((err) => {
@@ -35,13 +44,16 @@ connection.connect((err) => {
 
 });
 
-app.get('/home', (req, res) => {
-    res.render('home');
+app.get('/', (req, res) => {
+    res.redirect('/home');
 
 });
 
-app.get('/login', (req, res) => {
-    res.render('login');
+app.get('/home', async (req, res) => {
+    const loggedIn = req.session.loggedIn;
+    const username = req.session.username;
+
+    res.render('home', { loggedIn, username} );
 
 });
 
@@ -50,8 +62,20 @@ app.get('/register', (req, res) => {
 
 });
 
+app.get('/login', (req, res) => {
+    res.render('login');
+
+});
+
 app.post('/register', async (req, res) => {
     const { email, username, password, recaptchaResponse } = req.body;
+
+    async function isEmailUsed(email) {
+        const query = `SELECT COUNT(*) AS count FROM users WHERE email = ?`;
+        const [rows] = await connection.promise().query(query, [email]);
+
+        return rows[0].count > 0;
+    }
 
     try {
         const response = await axios.post('https://www.google.com/recaptcha/api/siteverify', null, {
@@ -68,8 +92,28 @@ app.post('/register', async (req, res) => {
             const salt = bcrypt.genSaltSync(10);
             const hashedPassword = bcrypt.hashSync(password, salt);
 
+            try {
+                const emailUsed = await isEmailUsed(email);
+                if(emailUsed) {
+                    console.log('Email is already used. User registration failed.');
+                    return res.redirect('register');
+                }
+
+                const query = `INSERT INTO users (username, email, password) VALUES (?, ?, ?)`;
+                await connection.promise().query(query, [username, email, hashedPassword]);
+
+                console.log('[NEW USER] ', { username, email, hashedPassword });
+
+            } catch(error) {
+                console.error('Error registering user: ', error);
+
+            }
+
             console.log('\nreCAPTCHA verification success');
-            res.redirect('home');
+
+            req.session.loggedIn = true;
+            req.session.username = username;
+            res.redirect('/home');
 
         } else {
             res.send('reCAPTCHA verification failed')
@@ -80,4 +124,45 @@ app.post('/register', async (req, res) => {
         res.status(500).send('Internal server error.');
     }
 
+});
+
+app.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        // Check if the email exists in the database
+        const query = `SELECT * FROM users WHERE email = ?`;
+        const [rows] = await connection.promise().query(query, [email]);
+
+        if (rows.length === 0) {
+            console.log('Email does not exist. User login failed.');
+            return res.redirect('/login');
+        }
+
+        const user = rows[0];
+
+        // Compare the entered password with the stored hashed password
+        const passwordMatch = bcrypt.compareSync(password, user.password);
+
+        if (!passwordMatch) {
+            console.log('Incorrect password. User login failed.');
+            return res.redirect('/login');
+        }
+
+        console.log('[LOGIN] User login successful: ', user.email);
+
+        // Redirect to the home page or any other desired route
+        req.session.loggedIn = true;
+        req.session.username = user.username;
+        res.redirect('/home');
+
+    } catch (error) {
+        console.error('Error during user login: ', error);
+        res.status(500).send('Internal server error.');
+    }
+});
+
+app.get('/logout', (req, res) => {
+    req.session.destroy();
+    res.redirect('/home');
 });
