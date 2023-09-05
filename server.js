ead462c4ecef665aa181a0d7a91fb6025814cf71
Mathys <mathys.nourry@edu.univ-fcomte.fr>
2023-09-05 22:44:29 +0200

    Edits

--- server.js
+++ server.js
@@ -207,3 +207,18 @@ app.get('/profile', async (req, res) => {
 
     res.render('profile', { loggedIn, email, username, hiddenPassword, role });
 });
+
+app.get('/dashboard', async (req, res) => {
+    const loggedIn = req.session.loggedIn;
+    const username = req.session.username;
+    const email = req.session.email;
+    let role;
+    if(loggedIn) {
+        // Get the role of the logged in user
+        const query = `SELECT role_name FROM roles WHERE id_role = (SELECT role_id FROM users WHERE email = ?)`;
+        const [rows] = await connection.promise().query(query, [email]);
+        role = rows[0].role_name;
+    }
+
+    res.render('dashboard', { loggedIn, username, role});
+});
