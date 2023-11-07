707b0e4dcf07c3264b3a9f4834b64cf7a2da5d31
Mathys <mathys.nourry@edu.univ-fcomte.fr>
2023-11-07 22:28:10 +0100

    Account edit pfp label done + need to fix the image itself

--- views/static/js/interactivePfpLabel.js
+++ views/static/js/interactivePfpLabel.js
@@ -1,18 +1,18 @@
-const input = document.getElementById("edit-pfp-choose-file");
-const label = document.getElementById("edit-pfp-choose-file-label");
+document.addEventListener('DOMContentLoaded', () => {
+    const input = document.getElementById("edit-pfp-choose-file");
+    const label = document.getElementById("edit-pfp-choose-file-label");
 
-let labelValue = label.innerHTML;
+    let labelValue = label.querySelector('#edit-pfp-filename').innerHTML;
 
-// FIXME: not working
+    input.addEventListener('change', function(e) {
+        let filename = input.value.split( '\\' ).pop();
+        console.log(`filename: ${filename}`);
 
-input.addEventListener('change', function(e) {
-    let filename = input.value.split( '/' ).pop();
-    console.log(`filename: ${filename}`);
-
-    if(filename) {
-        label.querySelector('span').innerHTML = filename;
-    } else {
-        label.innerHTML = labelValue;
-    }
+        if(filename) {
+            label.querySelector('#edit-pfp-filename').innerHTML = filename;
+            // document.getElementById("profile-picture").src =  URL.createObjectURL(input.value); // FIXME: image not uploading to the img element
+        } else {
+            label.innerHTML = labelValue;
+        }
+    });
 });
-
